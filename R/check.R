5872a2d7b33e2460d0d59115a28bfc0718b14f31
Antoine Fabri <antoine.fabri@gmail.com>
2021-10-21 16:52:38 +0200

    Put pj checks in own functions

--- R/check.R
+++ R/check.R
@@ -42,7 +42,135 @@ pj_check <- function(
     inconsistent_factor_levels, inconsistent_type)
 }
 
+check_duplicate_keys_left <- function(x, vars, check) {
+  if(!is.na(check[["duplicate_keys_left"]]) &&
+     n_distinct(x[vars$x$key]) != nrow(x)) {
+    fun <- getFromNamespace(check[["duplicate_keys_left"]], "rlang")
+    dupes <- x[vars$x$key]
+    dupes <- dupes[duplicated(dupes),]
+    msg <- paste0(
+      "Keys in the left table have duplicates:\n",
+      paste(capture.output(dupes), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
+
+check_duplicate_keys_right <- function(y, vars, check) {
+  if(!is.na(check[["duplicate_keys_right"]]) &&
+     n_distinct(y[vars$y$key]) != nrow(y)) {
+    fun <- getFromNamespace(check[["duplicate_keys_right"]], "rlang")
+    dupes <- y[vars$y$key]
+    dupes <- dupes[duplicated(dupes),]
+    msg <- paste0(
+      "Keys in the right table have duplicates:\n",
+      paste(capture.output(dupes), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
+
+check_missing_key_combination_left <- function(x, vars, check) {
+  if(!is.na(check[["missing_key_combination_left"]]) &&
+     n_distinct(x[vars$x$key]) != prod(vapply(x[vars$x$key], n_distinct, numeric(1)))) {
+    fun <- getFromNamespace(check[["missing_key_combination_left"]], "rlang")
+    all_combos <- exec(
+      "expand.grid",
+      !!! lapply(x[vars$x$key], unique),
+      stringsAsFactors = FALSE)
+    missing_combos <- setdiff(all_combos, x[vars$x$key])
+    missing_combos <- as_tibble(missing_combos)
+    msg <- paste0(
+      "Keys in the left table have missing combinations:\n",
+      paste(capture.output(missing_combos), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
 
 
+check_missing_key_combination_right <- function(y, vars, check) {
+  if(!is.na(check[["missing_key_combination_right"]]) &&
+     n_distinct(y[vars$y$key]) != prod(vapply(y[vars$y$key], n_distinct, numeric(1)))) {
+    fun <- getFromNamespace(check[["missing_key_combination_right"]], "rlang")
+    all_combos <- exec(
+      "expand.grid",
+      !!! lapply(y[vars$y$key], unique),
+      stringsAsFactors = FALSE)
+    missing_combos <- setdiff(all_combos, y[vars$y$key])
+    missing_combos <- as_tibble(missing_combos)
+    msg <- paste0(
+      "Keys in the right table have missing combinations:\n",
+      paste(capture.output(missing_combos), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
 
+check_inconsistent_factor_levels <- function(x_in, y_in, vars, check) {
+  if(!is.na(check[["inconsistent_factor_levels"]])) {
+    fun <- getFromNamespace(check[["inconsistent_factor_levels"]], "rlang")
+    if(check[["inconsistent_factor_levels"]] == "abort") {
+      msg <- mapply(function(x, y, x_nm, y_nm) {
+        if(is.factor(x) && is.factor(y) && !setequal(levels(x), levels(y))) {
+          if(x_nm == y_nm)
+            sprintf("`%s` has different factor levels in the left and right tables", x_nm)
+          else
+            sprintf("`%s` and `%s` (in resp. left and right table) have different factor levels", x_nm, y_nm)
+        }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
+    } else {
+      msg <- mapply(function(x, y, x_nm, y_nm) {
+        if(is.factor(x) && is.factor(y) && !setequal(levels(x), levels(y))) {
+          if(x_nm == y_nm)
+            sprintf("`%s` has different factor levels in the left and right tables, coercing to character vector", x_nm)
+          else
+            sprintf("`%s` and `%s` (in resp. the left and right table) have different factor levels, coercing to character vector", x_nm, y_nm)
+        }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
+    }
+    msg <- msg[lengths(msg)>0]
+    msg <- paste(msg, collapse = "\n")
+    fun(msg)
+  }
+}
 
+check_inconsistent_type <- function(x_in, y_in, vars, check) {
+if(!is.na(check[["inconsistent_type"]])) {
+  fun <- getFromNamespace(check[["inconsistent_type"]], "rlang")
+  msg <- mapply(function(x, y, x_nm, y_nm) {
+    if(!identical(typeof(x), typeof(y)) || !identical(class(x), class(y))) {
+      if(x_nm == y_nm)
+        sprintf("`%s` has a different type or class in the left and right tables", x_nm)
+      else
+        sprintf("`%s` and `%s` (in resp. left and right table) have different types or classes", x_nm, y_nm)
+    }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
+  msg <- msg[lengths(msg)>0]
+  if(length(msg)) {
+    msg <- paste(msg, collapse = "\n")
+    fun(msg)
+  }
+}
+}
+
+check_unmatched_keys_left <- function(x_in, y_in, vars, rows, check) {
+  if(!is.na(check[["unmatched_keys_left"]]) && length(rows$x_unmatched)) {
+    fun <- getFromNamespace(check[["unmatched_keys_left"]], "rlang")
+    unmatched_combos <- distinct(x_in[rows$x_unmatched, vars$x$key])
+    msg <- paste0(
+      "Keys in the left table have unmatched combinations:\n",
+      paste(capture.output(unmatched_combos), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
+
+check_unmatched_keys_right <- function(x_in, y_in, vars, rows, check) {
+  if(!is.na(check[["unmatched_keys_right"]]) && length(rows$y_unmatched)) {
+    fun <- getFromNamespace(check[["unmatched_keys_right"]], "rlang")
+    unmatched_combos <- distinct(y_in[rows$y_unmatched, vars$y$key])
+    msg <- paste0(
+      "Keys in the right table have unmatched combinations:\n",
+      paste(capture.output(unmatched_combos), collapse = "\n")
+    )
+    fun(msg)
+  }
+}
--- R/power_join_mutate.R
+++ R/power_join_mutate.R
@@ -54,11 +54,28 @@ join_mutate <- function(
   if(check[["implicit_by"]] %in% "abort") {
     abort("`by`is `NULL`, join columns should be explicit")
   }
+
   #-----------------------------------------------------------------------------
-  # inconsistent_type
-  # by needs to be explicited first
-  # check_inconsistent_type(x, y, by, check[["inconsistent_type"]])
-  #-----------------------------------------------------------------------------
+  # here we should check if ye have conflicts handled by the conflict arg
+  # if so we take out the conflicted variable(s) before this step
+  if(!is.null(conflict)) {
+    x_in_vars <- setdiff(names(x), names(by) %||% by)
+    y_in_vars <- setdiff(names(y), by)
+    # conflicts with by columns can't be fixed, they'll be handled by `column_conflict` check
+    conflicted_cols <- intersect(x_in_vars, y_in_vars)
+    if(is.list(conflict)) {
+      nms <- names(conflict)
+      if(any(!nms %in% conflicted_cols)) {
+        warn("Some conflict conditions are not used: ...")
+      }
+      conflicted_cols <- intersect(conflicted_colsm, nms)
+    }
+    conflicted_data <- list(x = x[conflicted_cols], y = y[conflicted_cols])
+    x <- x[!names(x) %in% conflicted_cols]
+    y <- y[!names(y) %in% conflicted_cols]
+  } else {
+    conflicted_data <- NULL
+  }
 
   vars <- join_cols(tbl_vars(x), tbl_vars(y),
                     by = by, suffix = suffix,
@@ -72,134 +89,22 @@ join_mutate <- function(
   y_in <- as_tibble(y, .name_repair = "minimal")
 
   #-----------------------------------------------------------------------------
-  # duplicate_keys_left
-  if(!is.na(check[["duplicate_keys_left"]]) &&
-     n_distinct(x[vars$x$key]) != nrow(x)) {
-    fun <- getFromNamespace(check[["duplicate_keys_left"]], "rlang")
-    dupes <- x[vars$x$key]
-    dupes <- dupes[duplicated(dupes),]
-    msg <- paste0(
-      "Keys in the left table have duplicates:\n",
-      paste(capture.output(dupes), collapse = "\n")
-    )
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # duplicate_keys_right
-  if(!is.na(check[["duplicate_keys_right"]]) &&
-     n_distinct(y[vars$y$key]) != nrow(y)) {
-    fun <- getFromNamespace(check[["duplicate_keys_right"]], "rlang")
-    dupes <- y[vars$y$key]
-    dupes <- dupes[duplicated(dupes),]
-    msg <- paste0(
-      "Keys in the right table have duplicates:\n",
-      paste(capture.output(dupes), collapse = "\n")
-    )
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # missing_key_combination_left
-  if(!is.na(check[["missing_key_combination_left"]]) &&
-     n_distinct(x[vars$x$key]) != prod(vapply(x[vars$x$key], n_distinct, numeric(1)))) {
-    fun <- getFromNamespace(check[["missing_key_combination_left"]], "rlang")
-    all_combos <- exec(
-      "expand.grid",
-      !!! lapply(x[vars$x$key], unique),
-      stringsAsFactors = FALSE)
-    missing_combos <- setdiff(all_combos, x[vars$x$key])
-    missing_combos <- as_tibble(missing_combos)
-    msg <- paste0(
-      "Keys in the left table have missing combinations:\n",
-      paste(capture.output(missing_combos), collapse = "\n")
-    )
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # missing_key_combination_right
-  if(!is.na(check[["missing_key_combination_right"]]) &&
-     n_distinct(y[vars$y$key]) != prod(vapply(y[vars$y$key], n_distinct, numeric(1)))) {
-    fun <- getFromNamespace(check[["missing_key_combination_right"]], "rlang")
-    all_combos <- exec(
-      "expand.grid",
-      !!! lapply(y[vars$y$key], unique),
-      stringsAsFactors = FALSE)
-    missing_combos <- setdiff(all_combos, y[vars$y$key])
-    missing_combos <- as_tibble(missing_combos)
-    msg <- paste0(
-      "Keys in the right table have missing combinations:\n",
-      paste(capture.output(missing_combos), collapse = "\n")
-    )
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # inconsistent_factor_levels
-  if(!is.na(check[["inconsistent_factor_levels"]])) {
-    fun <- getFromNamespace(check[["inconsistent_factor_levels"]], "rlang")
-    if(check[["inconsistent_factor_levels"]] == "abort") {
-      msg <- mapply(function(x, y, x_nm, y_nm) {
-        if(is.factor(x) && is.factor(y) && !setequal(levels(x), levels(y))) {
-          if(x_nm == y_nm)
-            sprintf("`%s` has different factor levels in the left and right tables", x_nm)
-          else
-            sprintf("`%s` and `%s` (in resp. left and right table) have different factor levels", x_nm, y_nm)
-        }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
-    } else {
-      msg <- mapply(function(x, y, x_nm, y_nm) {
-        if(is.factor(x) && is.factor(y) && !setequal(levels(x), levels(y))) {
-          if(x_nm == y_nm)
-            sprintf("`%s` has different factor levels in the left and right tables, coercing to character vector", x_nm)
-          else
-            sprintf("`%s` and `%s` (in resp. the left and right table) have different factor levels, coercing to character vector", x_nm, y_nm)
-        }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
-    }
-    msg <- msg[lengths(msg)>0]
-    msg <- paste(msg, collapse = "\n")
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # type
-  if(!is.na(check[["inconsistent_type"]])) {
-    fun <- getFromNamespace(check[["inconsistent_type"]], "rlang")
-    msg <- mapply(function(x, y, x_nm, y_nm) {
-      if(!identical(typeof(x), typeof(y)) || !identical(class(x), class(y))) {
-        if(x_nm == y_nm)
-          sprintf("`%s` has a different type or class in the left and right tables", x_nm)
-        else
-          sprintf("`%s` and `%s` (in resp. left and right table) have different types or classes", x_nm, y_nm)
-      }}, x_in[vars$x$key], y_in[vars$y$key], names(x_in[vars$x$key]), names(y_in[vars$y$key]))
-    msg <- msg[lengths(msg)>0]
-    if(length(msg)) {
-      msg <- paste(msg, collapse = "\n")
-      fun(msg)
-    }
-  }
+  # powerjoin checks
+  check_duplicate_keys_left(x, vars, check)
+  check_duplicate_keys_right(y, vars, check)
+  check_missing_key_combination_left(x, vars, check)
+  check_missing_key_combination_right(x, vars, check)
+  check_inconsistent_factor_levels(x_in, y_in, vars, check)
+  check_inconsistent_type(x_in, y_in, vars, check)
   #-----------------------------------------------------------------------------
   # original dplyr code
   x_key <- set_names(x_in[vars$x$key], names(vars$x$key))
   y_key <- set_names(y_in[vars$y$key], names(vars$y$key))
   rows <- join_rows(x_key, y_key, type = type, na_equal = na_equal)
   #-----------------------------------------------------------------------------
-  # unmatched_keys_left
-  if(!is.na(check[["unmatched_keys_left"]]) && length(rows$x_unmatched)) {
-    fun <- getFromNamespace(check[["unmatched_keys_left"]], "rlang")
-    unmatched_combos <- distinct(x_in[rows$x_unmatched, vars$x$key])
-    msg <- paste0(
-      "Keys in the left table have unmatched combinations:\n",
-      paste(capture.output(unmatched_combos), collapse = "\n")
-    )
-    fun(msg)
-  }
-  #-----------------------------------------------------------------------------
-  # unmatched_keys_right
-  if(!is.na(check[["unmatched_keys_right"]]) && length(rows$y_unmatched)) {
-    fun <- getFromNamespace(check[["unmatched_keys_right"]], "rlang")
-    unmatched_combos <- distinct(y_in[rows$y_unmatched, vars$y$key])
-    msg <- paste0(
-      "Keys in the right table have unmatched combinations:\n",
-      paste(capture.output(unmatched_combos), collapse = "\n")
-    )
-    fun(msg)
-  }
+  # powerjoin checks
+  check_unmatched_keys_left(x_in, y_in, vars, rows, check)
+  check_unmatched_keys_right(x_in, y_in, vars, rows, check)
   #-----------------------------------------------------------------------------
   # original dplyr code
   x_out <- set_names(x_in[vars$x$out], names(vars$x$out))
@@ -213,6 +118,14 @@ join_mutate <- function(
   }
   out <- vec_slice(x_out, x_slicer)
   out[names(y_out)] <- vec_slice(y_out, y_slicer)
+  #-----------------------------------------------------------------------------
+  # here we can slice our conflicted vars and add them to out
+  if(!is.null(conflicted_data)) {
+    res <- Map(as_function(conflict), conflicted_data$x, conflicted_data$x)
+    out[names(res)] <- res
+  }
+  #-----------------------------------------------------------------------------
+  # original dplyr code
   if (!keep) {
     key_type <- vec_ptype_common(x_key, y_key)
     out[names(x_key)] <- vec_cast(out[names(x_key)], key_type)
