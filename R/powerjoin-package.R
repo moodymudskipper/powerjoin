f2df5bd29d3bedcf4e8c5303810887b027c10cfc
Antoine Fabri <antoine.fabri@gmail.com>
2021-10-26 17:16:56 +0200

    Import vctrs functions explicitly

--- R/powerjoin-package.R
+++ R/powerjoin-package.R
@@ -1,10 +1,23 @@
 #' @import dplyr
 #' @importFrom tidyselect peek_vars matches
 #' @importFrom utils capture.output getFromNamespace
+#' @importFrom methods allNames
+#' @importFrom stats setNames
 #' @importFrom glue glue glue_collapse
-#' @import rlang vctrs
+#' @importFrom tidyr nest pivot_longer pivot_wider
+#' @importFrom purrr map map_lgl map_chr
+#' @importFrom vctrs vec_cast vec_ptype_common vec_slice vec_assign vec_equal_na
+#' @importFrom vctrs vec_group_loc vec_in vec_match vec_ptype_full vec_size
+#' @import rlang
 #' @keywords internal
 "_PACKAGE"
+
+globalVariables(c(
+  "type",
+  "data",
+  "name",
+  "value"
+))
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
