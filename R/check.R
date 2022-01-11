13197b7320a6945bcdc4c2e75ebb8fe43b0ef7ae
Antoine Fabri <antoine.fabri@gmail.com>
2022-01-11 13:33:33 +0100

    Document check_specs args

--- R/check.R
+++ R/check.R
@@ -18,11 +18,9 @@
 #'   all key combinations
 #' @param inconsistent_factor_levels what to do if the key columns from both sides
 #'   have inconsistent factor levels
-#'
-#'
-#' @param inconsistent_type WIP
-#' @param grouped_input WIP
-#' @param na_keys WIP
+#' @param inconsistent_type what to do if we joined keys have a different type
+#' @param grouped_input what to do if one or both of the tables are grouped
+#' @param na_keys what to do if keys contain missing values
 #'
 #' @export
 check_specs <- function(
